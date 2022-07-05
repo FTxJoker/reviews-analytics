043aa3ef17270c943579d9bc951a9687befc3695
Aaron <stay0913@gmail.com>
2022-07-05 13:46:03 +0800

    add flitering-2

--- reviews.py
+++ reviews.py
@@ -1,22 +1,28 @@
 data = []
 count = 0
 with open('reviews.txt', 'r') as f:
-    for line in f:
-        data.append(line)
+    for _ in f:
+        data.append(_)
         count += 1
         if count % 1000 == 0:
             print(len(data))
 print('檔案讀取完畢，共有 {0} 筆資料'.format(len(data)))
 
 sum_len = 0
-for d in data:
-    sum_len += len(d)
+for _ in data:
+    sum_len += len(_)
 print('每筆留言平均長度為 {0} 個字元'.format(sum_len / len(data)))
 
 new = []
-for d in data:
-    if len(d) < 100:
-        new.append(d)
+for _ in data:
+    if len(_) < 100:
+        new.append(_)
 print('一共有 {0} 筆留言長度小於100個字元'.format(len(new)))
 print(new[0])
 print(new[1])
+
+good = []
+for _ in data:
+    if 'good' in _:
+        good.append(_)
+print('一共有 {0} 筆留言包含good'.format(len(good)))
